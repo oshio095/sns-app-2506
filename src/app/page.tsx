cc157fbdba7f47c0adcf95f46571f4615a425350
oshio095 <oshio095@gmail.com>
2025-06-24 11:22:20 +0900

    Homeアクセス時にリダイレクト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-// import Image from "next/image";
+// src/app/page.tsx
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  return (
-    <div className="min-h-screen bg-red-300 flex items-center justify-center text-white text-2xl">
-      Tailwind CSS が効いていれば赤背景になります
-    </div>
-  );
+export default function Home() {
+  redirect("/login");
+  // return (
+    // <div className="min-h-screen bg-red-300 flex items-center justify-center text-white text-2xl">
+    //   Tailwind CSS が効いていれば赤背景になります
+    // </div>
+  // );
 }
